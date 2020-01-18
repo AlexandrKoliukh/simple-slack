596b6c9d71b64adc25ed8dbcc1f924142f18ee61
AlexandrKoliukh <alexkoliukh@gmail.com>
2020-01-18 16:07:35 +0300

    - add material - change design

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import {
+  List, ListItem, ListItemText
+} from '@material-ui/core';
 
 const App = (props) => {
   const { channels } = props;
 
   return (
-    <div>
-      <ul>
-        {channels.map((channel) => {
-          const { id, name } = channel;
-          return <li key={id}>{name}</li>;
-        })}
-      </ul>
-    </div>
+    <List component="nav" disablePadding>
+      {channels.map((channel) => {
+        const { id, name } = channel;
+        return (
+          <ListItem key={id}>
+            <ListItemText primary={name}/>
+          </ListItem>
+        );
+      })}
+    </List>
   );
 };
 
